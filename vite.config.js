2494afa4794b36928ad4bcf656c1dc5fac64861b
junperas <jhan.perez@correounivalle.edu.co>
2022-11-02 13:17:18 -0500

    Link de nuestra pagina en react-vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://junperas.github.io',//Link base de nuestro github
+  base:'https://junperas.github.io/actions-react-vite',//Link base de nuestro github
   plugins: [react()]
 })
